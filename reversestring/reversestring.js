f1ba5228e3a7274b5bec2e5eb252d092cf117098
Karthik <karthik@Karthiks-MacBook-Air.local>
2020-02-18 19:40:51 +0530

    reversestring

---
+++ reversestring/reversestring.js
@@ -0,0 +1,22 @@
+function reversestring(str){
+    let i = str.length-1;
+    let revstr = "";
+    while(i>=0){
+        revstr = revstr + str[i];
+        i--;
+    }
+    return revstr;
+}
+ let result = reversestring("praveen");
+ console.log(result);  
+
+ function reverse(str2){
+     let revstr2 = "";
+     for(let i=str2.length-1; i>=0; i--)
+     {
+        revstr2 = revstr2 + str2[i];
+     }
+     return revstr2;
+ }
+ let result2 = reverse("praveen");
+ console.log(result2);
