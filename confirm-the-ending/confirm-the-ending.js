7d89140e36c6a06aa53b824cae0c711ada303e1a
Karthik <karthik@Karthiks-MacBook-Air.local>
2020-03-02 12:29:41 +0530

    confirm-the-ending

---
+++ confirm-the-ending/confirm-the-ending.js
@@ -0,0 +1,28 @@
+function confirmEnding(str, target){
+    if(str.endsWith(target)){
+        return true;
+    }
+    return false;
+}
+console.log(confirmEnding("Bastian", "n"));
+function confirmEnding2(str, target){
+    return str.endsWith(target);
+}
+console.log(confirmEnding2("Bastian", "n"));
+function confirmEnding3(str, target){
+    if(str.substr(-target.length) === target){
+        return true;
+    }
+    return false;
+
+}
+console.log(confirmEnding3("Bastian", "n"));
+function confirmEnding4(str, target){
+    return str.substr(-target.length) === target;
+}
+console.log(confirmEnding4("Bastian", "n"));
+
+// substr
+// let sentence = "i am arun";
+// console.log(sentence.substr(2));
+// console.log(sentence.substr(-2));
